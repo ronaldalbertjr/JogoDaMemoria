08d8728e8572ae917cd0c9fd2ec2679be0fbaf74
Ronald Albert <ronaldalbert1609@gmail.com>
2016-09-13 14:27:59 -0300

    AlmostDoneWithTheScriptOfTheCards
    The Cards Are Almost Done

---
+++ MemoryGame/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour 
+{
+    public Sprite sp;
+    public GameObject equivalenteImage;
+
+    RawImage ima;
+
+    float time;
+    bool clicked = false;
+    bool bothClicked = false;
+	
+	void Update () 
+    {
+	    if(clicked)
+        {
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(0f, 180f, 0f), 0.1f);
+            if (this.transform.eulerAngles.y >= 90f)
+            {
+                this.GetComponent<Image>().sprite = sp;         
+            }
+            if(bothClicked)
+            {
+                this.GetComponent<Image>().fillAmount -= 0.01f;
+                equivalenteImage.GetComponent<Image>().fillAmount -= 0.01f;
+            }
+        }
+        if(this.GetComponent<Image>().fillAmount <= 0)
+        {
+            Destroy(this.gameObject);
+            Destroy(equivalenteImage);
+        }
+    }
+
+    public void OnClick()
+    {
+        clicked = true;
+        if(equivalenteImage.GetComponent<ButtonScript>().clicked)
+        {
+            Debug.Log("Both Clicked");
+            bothClicked = true;
+        }
+    }
+}
