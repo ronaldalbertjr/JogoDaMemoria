c5289ac34e77373648881eef1f769cec518a1714
Ronald Albert <ronaldalbert1609@gmail.com>
2016-09-15 22:45:57 -0300

    MadeTheCardsSpawnRandomly
    The Cards are now spawning randomly in the game

--- MemoryGame/Assets/Scripts/GameManagerScript.cs
+++ MemoryGame/Assets/Scripts/GameManagerScript.cs
@@ -8,10 +8,25 @@ using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour 
 {
-    List<GameObject> clickedCards = new List<GameObject>();
 
+    public Sprite[] sps;
+    List<GameObject> clickedCards = new List<GameObject>();
+    GameObject[] cards;
+    GameObject[] cards2;
     float turnTime;
     bool notEqui = false;
+
+    void Start()
+    {
+        cards = GameObject.FindGameObjectsWithTag("Cards");
+        cards2 = GameObject.FindGameObjectsWithTag("Cards2");
+        Shuffle(sps);
+        for(int i = 0; i < sps.Length; i++)
+        {
+            cards[i].GetComponent<ButtonScript>().sp = sps[i];
+            cards2[i].GetComponent<ButtonScript>().sp = sps[i];
+        }
+    }
 
 	void Update () 
     {
@@ -48,5 +63,20 @@ public class GameManagerScript : MonoBehaviour
     public void click(GameObject card)
     {
         clickedCards.Add(card);
+    }
+    
+    void Shuffle(Sprite[] sprites)
+    {
+
+        System.Random r = new System.Random();
+
+        for(int i = 0; i < sprites.Length; i++)
+        {
+            int idx = r.Next(0, sprites.Length);
+
+            Sprite tmp = sprites[i];
+            sprites[i] = sprites[idx];
+            sprites[idx] = tmp;
+        }
     }
 }
