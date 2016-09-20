838d7697a48c4b649b4775ebaa980b05db82b7f6
Ronald Albert <ronaldalbert1609@gmail.com>
2016-09-20 13:58:51 -0300

    MadeanEntireBackgroundToTheMemoryGame

---
+++ MemoryGame/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour 
+{
+    public bool movement = false;
+    public Transform camPos;
+	void Update () 
+    {
+        if(movement)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, camPos.position, 1000);
+            this.transform.eulerAngles = Vector3.MoveTowards(this.transform.eulerAngles, camPos.eulerAngles, 10); 
+        }	
+	}
+}
---
+++ MemoryGame/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour 
+{
+    public GameObject cam;
+
+    public void OnPlay()
+    {
+        cam.GetComponent<CameraScript>().movement = true;
+    }
+}
